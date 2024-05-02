8304a9bead495084705d68c64d3637a138f09b47
eddiepeters00 <eddie.peters00@outlook.com>
2024-05-02 11:34:01 +0200

    Added mongodb to project

---
+++ src/lib/mongoDb/users.ts
@@ -0,0 +1,41 @@
+import { Collection } from "mongodb";
+import clientPromise from ".";
+
+let users: Collection<User>;
+type User = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+async function init() {
+  try {
+    const client = await clientPromise;
+    const db = await client.db();
+    users = await db.collection<User>("users");
+  } catch (error) {
+    throw new Error("Failed to stablish connection to database");
+  }
+}
+
+(async () => {
+  await init();
+})();
+
+/////////////
+/// USERS ///
+/////////////
+
+export async function getUsers() {
+  try {
+    if (!users) await init();
+    const result = await users
+      .find({})
+      .map((user) => ({ ...user, _id: user._id.toString() }))
+      .toArray();
+
+    return { users: result };
+  } catch (error) {
+    return { error: "Failed to fetch users" };
+  }
+}
