a6388ccf1c8fdb1fb1bda55301a565b0f43d146b
eddiepeters00 <eddie.peters00@outlook.com>
2024-05-06 13:24:25 +0200

    Fetch all gyms and display them in /gyms

--- src/app/(authed)/gyms/page.tsx
+++ src/app/(authed)/gyms/page.tsx
@@ -1,10 +1,10 @@
-import { getUsers } from "@/lib/mongoDb/users";
 import Image from "next/image";
 import man_bouldering from "../../../../public/shirtless-sporty-male-climbing-indoor-climbing-wall.jpg";
 import { Card, CardTitle } from "@/components/ui/card";
+import { getGyms } from "@/lib/mongoDb/gyms";
 
 export default async function Gyms() {
-  const users = await getUsers();
+  const gyms = await getGyms();
   return (
     <section>
       <Image
@@ -14,10 +14,10 @@ export default async function Gyms() {
       />
 
       <ul className="grid gap-6 mx-4 pt-8">
-        {users.users?.map((user) => (
-          <li key={user._id}>
+        {gyms.gyms?.map((gym) => (
+          <li key={gym._id}>
             <Card className=" flex justify-between items-center text-black h-10 p-2">
-              <CardTitle className="text-lg font-medium">{user.name}</CardTitle>
+              <CardTitle className="text-lg font-medium">{gym.name}</CardTitle>
               <span className="">12/32</span>
             </Card>
           </li>
