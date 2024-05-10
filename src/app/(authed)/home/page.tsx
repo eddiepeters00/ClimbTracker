d9c9047ae78fbdb5e7104a35e6f2916ae4015151
eddiepeters00 <eddie.peters00@outlook.com>
2024-05-10 10:53:22 +0200

    Added settings dropdown in header

--- src/app/(authed)/home/page.tsx
+++ src/app/(authed)/home/page.tsx
@@ -1,14 +1,8 @@
 import { getGyms } from "@/lib/mongoDb/gyms";
-import Signout from "./signout";
 import Map from "@/components/map/map";
 
 export default async function Page() {
   const gyms = await getGyms();
 
-  return (
-    <>
-      {gyms.gyms && <Map gyms={gyms.gyms} />}
-      <Signout />
-    </>
-  );
+  return <>{gyms.gyms && <Map gyms={gyms.gyms} />}</>;
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,38 @@
 "use client";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogOut, Settings } from "lucide-react";
+import { signOut } from "next-auth/react";
+
 export default function Header() {
   return (
-    <header className="bg-primary-foreground  p-2 grid place-content-center min-w-full text-center static">
+    <header className="bg-primary-foreground  p-2 grid place-content-center min-w-full text-center relative">
       <h1 className="text-primary font-bold text-xl">Climb Tracker</h1>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="absolute right-0 top-0 p-2 text-black">
+          <Settings />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel className="text-black">
+            My Account
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            className="flex items-center gap-2 text-black"
+            onClick={() => signOut({ callbackUrl: "/" })}
+          >
+            <LogOut className="p-1" />
+            Sign out
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </header>
   );
 }
