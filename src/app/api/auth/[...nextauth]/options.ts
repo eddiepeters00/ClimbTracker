767b336bf227a74894bb98a2112b5cba254d2598
eddiepeters00 <eddie.peters00@outlook.com>
2025-01-31 15:03:55 +0100

    Added facebook provider for authentication

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,6 @@
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook"
 import { addNewUser } from "@/lib/mongoDb/users";
 
 export const options: NextAuthOptions = {
@@ -7,6 +8,10 @@ export const options: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
     }),
   ],
 
